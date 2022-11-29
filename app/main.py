491ffeff1b3da31d2e18044f583a98a98703a3d3
Artyom Kropp <artyomkropp@gmail.com>
2022-11-30 02:46:18 +0900

    remove static

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 
-from fastapi.staticfiles import StaticFiles
-
 from app.routers import index
 
 app = FastAPI()
-
-app.mount('/static', StaticFiles(directory='static'), name='static')
 
 app.include_router(index.router)
