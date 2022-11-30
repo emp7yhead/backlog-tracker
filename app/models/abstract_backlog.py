bc4deadcff5837b7125c2f4bee2f9a8a9ae0bc4b
Artyom Kropp <artyomkropp@gmail.com>
2022-12-01 02:38:07 +0900

    add models

---
+++ app/models/abstract_backlog.py
@@ -0,0 +1,12 @@
+from typing import ClassVar
+from sqlalchemy import Column, Integer, String, Time
+
+from app.db.session import Base
+
+
+class AbstractBacklog(Base):
+    __abstract__: ClassVar[bool] = True
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String())
+    required_time = Column(Time)
