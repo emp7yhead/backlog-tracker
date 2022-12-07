8b19dd6aac4fa6c74ac6b6467da345120e5a8946
Artyom Kropp <artyomkropp@gmail.com>
2022-12-08 02:34:18 +0900

    add route tag

--- backlog_tracker/routers/index.py
+++ backlog_tracker/routers/index.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-router = APIRouter()
+router = APIRouter(
+    tags=['index'],
+)
 
 templates = Jinja2Templates(directory='templates')
 
