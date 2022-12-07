37d517d69b186105c56c237f0a7bd84d655006e4
Artyom Kropp <artyomkropp@gmail.com>
2022-12-08 02:33:54 +0900

    fix user model

--- backlog_tracker/models/user.py
+++ backlog_tracker/models/user.py
@@ -5,8 +5,9 @@ from backlog_tracker.db.session import Base
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
+    email = Column(String)
 
     def __repr__(self):
         return f"<User({self.name})>"
