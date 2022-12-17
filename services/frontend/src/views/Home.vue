68dd8cb1e7bbfaf1fddd4d774ab57fc977e987b2
Artyom Kropp <artyomkropp@gmail.com>
2022-12-18 02:10:05 +0900

    remove get message for main page

--- services/frontend/src/views/Home.vue
+++ services/frontend/src/views/Home.vue
@@ -34,30 +34,3 @@
       </div>
     </main>
 </template>
-
-<script>
-import axios from 'axios';
-
-export default {
-  name: 'Ping',
-  data() {
-    return {
-      msg: '',
-    };
-  },
-  methods: {
-    getMessage() {
-      axios.get('/')
-        .then((res) => {
-          this.msg = res.data;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
-  },
-  created() {
-    this.getMessage();
-  },
-};
-</script>
