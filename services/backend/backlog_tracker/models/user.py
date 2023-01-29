ae1f7ae8d3cf6970a5b6a5c6e739a4c1f4db0d9b
emp7yhead <artyomkropp@gmail.com>
2023-01-30 00:28:36 +0600

    update user model

--- services/backend/backlog_tracker/models/user.py
+++ services/backend/backlog_tracker/models/user.py
@@ -1,13 +1,17 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, UniqueConstraint
 from backlog_tracker.db.session import Base
 
 
 class User(Base):
     __tablename__ = 'users'
+    __table_args__ = (
+        UniqueConstraint('username', 'email'),
+    )
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String)
-    email = Column(String)
+    username = Column(String(length=20))
+    email = Column(String(length=20))
+    password = Column(String(length=128))
 
     def __repr__(self):
         return f"<User({self.name})>"
