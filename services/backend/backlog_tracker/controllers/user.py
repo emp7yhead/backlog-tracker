2c921d89205e4c04626990323e25156fae667823
emp7yhead <artyomkropp@gmail.com>
2023-02-05 21:38:27 +0600

    update deprecated methods

--- services/backend/backlog_tracker/controllers/user.py
+++ services/backend/backlog_tracker/controllers/user.py
@@ -1,4 +1,5 @@
 from typing import List
+from sqlalchemy import delete, select, update
 from passlib.context import CryptContext
 
 from backlog_tracker.models.user import User
@@ -12,38 +13,56 @@ class UserController:
         self.session = session
 
     def get_user(self, user_id: int) -> UserOut:
-        user = self.session.query(User).get(user_id)
+        user = self.session.get(User, user_id)
         return user
 
     def get_user_by_username(self, username: str) -> UserOut:
-        user = self.session.query(User).filter(
-            User.username == username
-        ).first()
+        user = self.session.execute(
+            select(User).
+            where(User.username == username)
+        ).all()
         return user
 
     def get_all_users(self, page, limit, offset) -> List[UserOut]:
-        all_users = self.session.query(User).limit(limit).offset(
-            offset * (page - 1)
+        all_users = self.session.scalars(
+            select(User).
+            limit(limit).
+            offset(offset * (page - 1))
         ).all()
         return all_users
 
     def create_user(self, user: UserIn) -> UserOut:
-        user.password = password_context.encrypt(user.password)
+        user.password = password_context.hash(user.password)
         new_user = User(**user.dict())
         self.session.add(new_user)
         self.session.commit()
         return new_user
 
     def update_user(self, user_id: int, user: UserIn) -> UserOut:
-        user_to_update = self.session.query(User).get(user_id)
-        user_to_update.username = user.username
-        user_to_update.password = password_context.encrypt(user.password)
-        self.session.add(user_to_update)
-        self.session.commit()
+        user_to_update = self.session.get(User, user_id)
+        self.session.execute(
+            update(User).
+            where(User.id == user_id).
+            values(
+                username == user.username,
+                password == password_context.hash(user.password),
+            ).
+            execution_options(
+                synchronize_session="evaluate"
+            )
+        )
+        # user_to_update.username = user.username
+        # user_to_update.password = password_context.encrypt(user.password)
+        # self.session.add(user_to_update)
+        # self.session.commit()
         return user_to_update
 
     def delete_user(self, user_id: int) -> UserOut:
-        user = self.session.query(User).get(user_id)
-        self.session.delete(user)
+        user = self.session.get(User, user_id)
+        self.session.execute(
+            delete(User)
+            .where(User.id == user_id)
+            .execution_options(synchronize_session="fetch")
+        )
         self.session.commit()
         return user
--- services/backend/backlog_tracker/db/session.py
+++ services/backend/backlog_tracker/db/session.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 from backlog_tracker.settings import settings
 
 engine = create_engine(
